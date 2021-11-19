75e51b16542143fc047500e389d6f4cc8135d228
shankar Ghimire <ssghimire@gmail.com>
2021-11-19 09:15:14 -0500

    Adds the data-binding property to the sign up form

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+public user={
+  userName: '',
+  password: '',
+  firstName:'',
+  lastName:'',
+  email:'',
+  phone:'',
+};
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  formSubmit(){
+    //alert("submitted!");
+    if(this.user.userName === '' || this.user.userName == null){
+      alert('Username is required!');
+      return;
+    }
+    console.log(this.user);
+  }
 }
